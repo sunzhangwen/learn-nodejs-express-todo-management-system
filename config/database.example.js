626dd9db3a4faffcf9996d07bba828614bcc5a92
sunzhangwen <szw713@hotmail.com>
2026-06-18 23:21:20 +0800

    add database.example.js and gitignore database.js with credentials

--- config/database.example.js
+++ config/database.example.js
@@ -1,6 +1,6 @@
 const {Sequelize} = require('sequelize')
 
-const sequelize = new Sequelize('{your db name}', '{your db user id}', '{user db user password}', {
+const sequelize = new Sequelize('your_database_name', 'your_username', 'your_password', {
     host:'localhost',
     dialect:'mysql',
     logging:false
